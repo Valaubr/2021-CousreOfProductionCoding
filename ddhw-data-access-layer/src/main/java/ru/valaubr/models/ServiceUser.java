9eb13397ebefd4afad7ffde37222a3c1093c150c
Valaubr <stalker039906@gmail.com>
2021-05-03 21:51:18 +0300

    Add Hibernate ORM

--- ddhw-data-access-layer/src/main/java/ru/valaubr/models/ServiceUser.java
+++ ddhw-data-access-layer/src/main/java/ru/valaubr/models/ServiceUser.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 import ru.valaubr.enums.Role;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
 @Getter
 @Setter
+@Entity
 public class User {
     private String fName;
     private String lName;
     private String password;
+    @Id
     private String Email; //Primary-key like login
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
